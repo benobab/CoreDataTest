ca8bbd258154eafdef2f6e229b0ba9b63b6295b7
benobab <benlacroix83@gmail.com>
2014-12-15 16:09:40 +0100

    Final App

--- CoreDateTest/ViewController.swift
+++ CoreDateTest/ViewController.swift
@@ -7,9 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    //Storyboard Text Field
+    @IBOutlet weak var userNameTextFIeld: UITextField!
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    
+    
+    //Méthodes des boutons de la storyBoard
+    @IBAction func loadButtonPressed(sender: UIButton) {
+        //pour avoir le contexte actuel (BDD quoi)
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        var request = NSFetchRequest(entityName: "Users")
+        request.returnsObjectsAsFaults = false
+        
+        var result: NSArray = context.executeFetchRequest(request, error: nil)!
+        
+        if(result.count > 0)
+        {
+            var res = result[0] as NSManagedObject
+            userNameTextFIeld.text = res.valueForKey("username") as String
+            passWordTextField.text = res.valueForKey("password") as String
+        }
+        
+        
+    }
+    @IBAction func saveButtonPressed(sender: UIButton) {
+        var appDel:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+        var context:NSManagedObjectContext = appDel.managedObjectContext!
+        
+        //création d'un utilisateur via xcdatamodel
+        var newUser = NSEntityDescription.insertNewObjectForEntityForName("Users", inManagedObjectContext: context) as NSManagedObject
+        newUser.setValue("" + userNameTextFIeld.text, forKey: "username")
+        newUser.setValue("" + passWordTextField.text, forKey: "password")
+        context.save(nil)
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
